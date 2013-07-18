7446938fecfd019c29640887950d04b2e9daccf1
Malik <malik37@fullsail.edu>
2013-07-18 16:38:50 -0400

    This change adds more code being included for both the Dog Years and Slice of Pie 1

--- WPF/Activites/AssociatingJS/Week 2/Expression_worksheet/script.js
+++ WPF/Activites/AssociatingJS/Week 2/Expression_worksheet/script.js
@@ -5,9 +5,21 @@
 
 // Dog Years
 
-	var humanAge = 10; // The dog's age in human years. 
+var humanAge = 10; // The dog's age in human years. 
 
-	var dogAge = humanAge * 7; // The dog's age in human years multiplied by 7 to represent dog's actual age. 
-	console.log("The dog's real age is", dogAge); // Printing out the representation of the dog's true age to the console.
+var dogAge = humanAge * 7; // The dog's age in human years multiplied by 7 to represent dog's actual age. 
+console.log("The dog's real age is", dogAge); // Printing out the representation of the dog's true age to the console.
 
+
+// Slice of Pie I 
+
+var amountOfSlices = 8;
+var amountOfPeople = 70; 
+var amountOfPizzas = 50;
+
+// pizza * slices / people 
+
+var pizzaEaten = amountOfPizzas * amountOfSlices / amountOfPeople;
+
+console.log("Each person ate" , pizzaEaten, "slices at the party");
 
