437b17f7553f474ad991b9f89a1963b14bd28ebb
Malik <malik37@fullsail.edu>
2013-07-18 18:05:47 -0400

    Adds another problem about a Discounted item.
    This commit consists of a price being identified, given a discount,
    then adding taxes to it revealing the after price.

--- WPF/Activites/AssociatingJS/Week 2/Expression_worksheet/script.js
+++ WPF/Activites/AssociatingJS/Week 2/Expression_worksheet/script.js
@@ -30,11 +30,36 @@ console.log("Each person ate" , pizzaEaten, "slices at the party");
 	var sparkyGot = 20 % 6;
 	console.log("Sparky got" , sparkyGot, "slices of pizza");
 
-	// Average Shopping Bill
+// Average Shopping Bill
 
 	var bill = [50, 70, 30, 100, 20];
 	var totalSpent = bill[0] + bill[1] + bill[2] + bill[3] + bill[4];
 	var average = totalSpent / 5;
 
 	console.log("You've spent a total of $" , totalSpent, "on groceries over 5 weeks. This brings it to a total of $" , average, "per week.")
+
+// Discounts
+
+var itemName = "milk"; 
+var itemDescription = "skim";
+var itemPrice = "4";
+var discount = "50";
+var tax = "3";
+var itemDiscount = discount / 100;
+var itemTax = tax / 100;
+
+// Item deal with discount first without tax.
+
+discountPrice = itemPrice * itemDiscount;
+
+discountInclude = itemPrice - discountPrice; 
+
+// Tax included after discount is applied. 
+
+taxPrice = discountInclude * itemTax;
+taxInclude = taxPrice + discountInclude; 
+
+console.log("The" , itemName, "was at first $" , itemPrice, "but after the" , discount , "% discount, it is now $" , discountInclude, "without the tax and $" , taxInclude, "with the tax.");
+
+
 
