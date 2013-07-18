138afa37b632604f9d873241a27ba861d210fa97
Malik <malik37@fullsail.edu>
2013-07-18 16:31:28 -0400

    This is the initial push of my files from my new Repository.
    Emergency repository made because of issues with first one.

---
+++ AssociatingJS/Expression_worksheet/script.js
@@ -0,0 +1,13 @@
+/* User: Henry Malik
+   Date : July 17 2013
+   Assignment: Expression Worksheet */
+
+
+// Dog Years
+
+	var humanAge = 10; // The dog's age in human years. 
+
+	var dogAge = humanAge * 7; // The dog's age in human years multiplied by 7 to represent dog's actual age. 
+	console.log("The dog's real age is", dogAge); // Printing out the representation of the dog's true age to the console.
+
+
---
+++ AssociatingJS/js/script.js
@@ -0,0 +1,26 @@
+alert("This is in my JavaScript File!");
+
+/*var yearBorn = 1994; // This is the year I was born.
+
+var age = 2013 - yearBorn; // This will equal to how old I am currently. 
+
+alert(age); //This will show my age in an alert box. 
+
+var yearOfHugo = 1989;
+
+var age = yearOfHugo + 30;
+
+alert(age);
+
+var ruin = age;
+
+var born = age - yearOfHugo;
+
+alert(born); */
+
+// The area of a triangle is half the width times the height. 
+
+var remainder = 55 % 3
+var age; 
+var age; 
+console.log(remainder);
