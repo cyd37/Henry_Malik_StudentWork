0391259f713b8657c14d506f60a142ea6d9d735e
Malik <malik37@fullsail.edu>
2013-07-18 22:21:04 -0400

    This commit changes the file name of the industry expression file to industry.js and this also initiates the initial industry specified expression file.

--- WPF/Activites/AssociatingJS/Week 2/Expressions/Industry/industry.js
+++ WPF/Activites/AssociatingJS/Week 2/Expressions/Industry/industry.js
@@ -1,4 +1,12 @@
 /* Henry Malik
 July 17 2013
-Expression Worksheet- Dog Years */
+Expression Worksheet- Web Design Schedule */
 
+// This shows the amount of time you would spend working on a main client design and code pages written per year.
+alert("This is a test")
+var hoursPerDay = prompt("How many hours do you spend with your client everyday?:");
+var codePagesPerDay = prompt("How many pages of codes do you write for your client each day?:"); 
+totalHours = hoursPerDay * 365;
+totalPages = codePagesPerDay * 365;
+
+console.log("Since you spend" hoursPerDay "per day with your client for the year, you would have worked" totalHours "hours in the year for that client. Also, since you wrote" codePagesPerDay "pages of codes each day for a year, you would have wrote" totalPages "codes of pages in the year for that main client.");
