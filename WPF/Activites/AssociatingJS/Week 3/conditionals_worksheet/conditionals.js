c42eb3c94118b3280c5e941ae979bf2ae3880c8c
Malik <malik37@fullsail.edu>
2013-07-24 17:23:07 -0400

    Initial Commit of Conditionals Worksheet JS File and Conditionals Worksheet HTML File

---
+++ WPF/Activites/AssociatingJS/Week 3/conditionals_worksheet/conditionals.js
@@ -0,0 +1,25 @@
+/* Malik Henry
+   Web Programming Fundamentals
+   Mr. Joey Dalton
+   July 24, 2013
+   Assignment: Conditionals Worksheet
+ */
+ 
+
+//Example
+
+//To get in the heavyweight division of the Strawberry Festival’s pie eating contest you must weigh 250 lbs or more.  Determine whether an entrant qualifies based on his weight.
+
+// To see whether or not the competitor is equal to 250 lb or more.
+
+var competitorWeight = 249; 
+
+var minWeight = 249
+
+if(competitorWeight >= 250){ console.log("The Competitor is elligble!")
+
+}else{}
+
+if(competitorWeight <= minWeight) {console.log("You need to gain more weight")}
+
+
