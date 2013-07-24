64a634529764c75519844c04987c7ec5595fd24c
Malik <malik37@fullsail.edu>
2013-07-24 19:22:29 -0400

    This added more comments to the code blocks being executed.

--- WPF/Activites/AssociatingJS/Week 3/conditionals_worksheet/js/conditionals.js
+++ WPF/Activites/AssociatingJS/Week 3/conditionals_worksheet/js/conditionals.js
@@ -23,22 +23,3 @@ if(competitorWeight >= 250){ console.log("The Competitor is elligble!")
 if(competitorWeight <= minWeight) {console.log("You need to gain more weight")}
 
 
-//Fahrenheit To Celsius
-
-//Given
-
-var temp = 77; // This is 77 F
-
-var converted = "C"; // This is what the temperature will be converted to.
-
-if(converted === "F") {    //This shows that it has to be strict equality. 
-
-	tempToF = temp * 9 / 5 + 32
-	console.log(tempToF); // If the selection is F, it will use this block of code.
-}else{
-
-	tempToC = (temp - 32) * 5 / 9 // If the selection is C, this block of code will be used.
-	console.log(tempToC);
-}
-
-
