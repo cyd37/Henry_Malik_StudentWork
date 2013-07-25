e34fe7ba0a81a1f6875478acc60c0d102cc6b750
Malik <malik37@fullsail.edu>
2013-07-25 03:10:07 -0400

    Added a next worksheet about a Graded Letter Calculated. And also corrected the spelling in elligible for the example exercise.

--- WPF/Activites/AssociatingJS/Week 3/conditionals_worksheet/conditionals.js
+++ WPF/Activites/AssociatingJS/Week 3/conditionals_worksheet/conditionals.js
@@ -16,7 +16,7 @@ var competitorWeight = 249;
 
 var minWeight = 249
 
-if(competitorWeight >= 250){ console.log("The Competitor is elligble!")
+if(competitorWeight >= 250){ console.log("The Competitor is elligible!")
 
 }else{}
 
@@ -41,4 +41,30 @@ if(converted === "F") {    //This shows that it has to be strict equality.
 	console.log(tempToC);
 }
 
+//Grade Letter Calculator
 
+var gradeNumber = prompt ("What grade did you receive at the end of your course as a whole number?");
+    
+if (gradeNumber >= 95){
+    console.log("You received a " + gradeNumber + " which results to an A+"); //If the grade is a 95 or better, it will be an A+.
+    
+} else if(gradeNumber <= 94 && gradeNumber >= 90){
+    console.log("You received a " + gradeNumber + " which results to an A."); //If the grade is greater than an 89 but less than a 95, it's an A.
+    
+} else if(gradeNumber <= 89 && gradeNumber >= 85){
+    console.log("You received a " + gradeNumber + " which results to a B+."); //If the grade is greater than 84 but less than 90, it's an B+.
+    
+} else if(gradeNumber <= 84 && gradeNumber >= 80){
+    console.log("You received a " + gradeNumber + " which results to a B."); //If the grade is greater than 79 but less than 85, it's an B.
+    
+} else if(gradeNumber <= 79 && gradeNumber >= 76){
+    console.log("You received a " + gradeNumber + " which results to a C+."); //If the grade is greater than 75 but less than 80, it's an C+.
+    
+} else if(gradeNumber <= 75 && gradeNumber >= 73){
+    console.log("You received a " + gradeNumber + " which results to a C."); //If the grade is greater than 72 but less than 76, it's an C.
+   
+} else if(gradeNumber <= 72 && gradeNumber>= 70){
+    console.log("You received a " + gradeNumber + " which results to a D.");  //If the grade is greater than 69 but less than 73, it's an D.
+    
+} else
+    console.log("You received a " + gradeNumber + " which results to a failure."); //This returns to a failure.
