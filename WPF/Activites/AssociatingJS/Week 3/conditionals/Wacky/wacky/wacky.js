5c0bf7d0b01c26297c9072a519f2045562e69b60
Malik <malik37@fullsail.edu>
2013-07-25 21:20:56 -0400

    This commit pushes the initial script for my wacky conditions.

--- WPF/Activites/AssociatingJS/Week 3/conditionals/Wacky/wacky/wacky.js
+++ WPF/Activites/AssociatingJS/Week 3/conditionals/Wacky/wacky/wacky.js
@@ -5,6 +5,25 @@
 */	
 
 
-// My Personal Condition
+// My Wacky Condition
+
+// This conditional script shows the average amount of sleep a human being gets in a lifetime. 
+
+
+
+var daysInYear = 365;
+var lifeSpan = 78.6;
+var yourNumber = prompt("How much sleep do you get a night? (Answer in a whole number)");
+var averageSleep = yourNumber;
+var sleepAYear = yourNumber * daysInYear;
+var averageSleep = averageSleep * daysInYear * lifeSpan;
+console.log("You sleep an average of", yourNumber,"hours a night. Which in a year totals out to be", sleepAYear," hours a year and",averageSleep, " hours in your lifetime.");
+
+
+
+
+//average sleep 7
+//average lifespan 78.6 
+// 365 a year
 
 
