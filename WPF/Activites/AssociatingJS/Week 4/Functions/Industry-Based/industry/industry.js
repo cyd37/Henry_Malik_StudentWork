9a2b99c1bd2a1217b49a8d335c315af3d5dcf649
Malik <malik37@fullsail.edu>
2013-08-01 18:58:52 -0400

    Adds the rest of the functionality to the code.

--- WPF/Activites/AssociatingJS/Week 4/Functions/Industry-Based/industry/industry.js
+++ WPF/Activites/AssociatingJS/Week 4/Functions/Industry-Based/industry/industry.js
@@ -24,3 +24,13 @@ download.onload = function ()
 	download.src = imageAddr; // Downloading the content assigned to the imageAddr variable.
 
 function showResults() // Defines bandwith related variables and perform mathematical operation then displays results.
+
+{ 
+	
+		var duration = (endTime - startTime) / 1000;
+		var bitsLoaded = downloadSize * 8;
+		var speedBps = (bitsLoaded /duration).toFixed(2);
+	    var speedKbps = (speedBps /1024).toFixed(2); 
+		var speedMbps = (speedKbps /1024).toFixed(2); 
+		   alert("Your connection speed is: \n" + speedBps + " bps\n" + speedKbps + " kbps\n" + speedMbps + " Mbps\n" ); 
+}
