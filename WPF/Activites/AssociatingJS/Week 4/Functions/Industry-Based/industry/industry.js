0ab4b79ecf7d889bf145f6242ce32daec08c1917
Malik <malik37@fullsail.edu>
2013-08-01 18:57:38 -0400

    Adds more function information and shows what it does.

--- WPF/Activites/AssociatingJS/Week 4/Functions/Industry-Based/industry/industry.js
+++ WPF/Activites/AssociatingJS/Week 4/Functions/Industry-Based/industry/industry.js
@@ -15,3 +15,12 @@ var startTime, endTime; // Creating two more variables to mark start and end of 
 var downloadSize = 200000; // Creating a next variable and assigning it the value of your download size.
 var download = new Image(); // Creates variable and assigned to 
 download.onload = function ()
+
+{
+	 endTime = (new Date()).getTime(); // Using the onload function and it's defining the time variables created earlier. 
+	showResults();
+}
+		 startTime = (new Date()).getTime(); // Defining the start time variable using the date and time command. 
+	download.src = imageAddr; // Downloading the content assigned to the imageAddr variable.
+
+function showResults() // Defines bandwith related variables and perform mathematical operation then displays results.
