7cd287bedbd7de266cb8b3874781d492ff9573e6
Malik <malik37@fullsail.edu>
2013-08-01 20:01:03 -0400

    Added another comment.

--- WPF/Activites/AssociatingJS/Week 4/Functions/Wacky/wacky/wacky.js
+++ WPF/Activites/AssociatingJS/Week 4/Functions/Wacky/wacky/wacky.js
@@ -20,8 +20,7 @@ var wordBank = {
 
  word3 : ['apple-john','baggage','barnacle','bladder','boar-pig','bugbear','bum-bailey','canker-blossom','clack-dish','clotpole','coxcomb','codpiece','death-token','dewberry','flap-dragon','flax-wench','flirt-gill','foot-licker','fustilarian','giglet','gudgeon','haggard','harpy','hedge-pig','horn-beast','hugger-mugger','joithead','lewdster','lout','maggot-pie','malt-worm','mammet','measle','minnow','miscreant','moldwarp','mumble-news','nut-hook','pigeon-egg','pignut','puttock','pumpion','ratsbane','scut','skainsmate','strumpet','varlot','vassal','whey-face','wagtail']
 
-};
-//set up variable to hold our insult
+}; //set up variable to hold the insult. 
 
 var funnyInsults;
 //set up and get a random word for each section of our insult
